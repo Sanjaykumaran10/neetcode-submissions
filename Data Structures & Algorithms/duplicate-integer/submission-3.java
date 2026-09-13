5ca48baf630451e77a5c283b426f619c96597e44
Sanjay Kumaran <79367594+Sanjaykumaran10@users.noreply.github.com>
2026-09-13 20:26:35 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set <Integer> n = new HashSet<>();
+        for(int i : nums){
+            if(n.contains(i)){
+                return true;
+            }
+            n.add(i);
+        }
+        return false;
+    
+         }
+}
